d628eaa38b23015ac4f5e7b284d3e42775d11755
Landen Danyluk <landendanyluk@gmail.com>
2018-07-03 16:15:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length) {
     return `Currently serving ${katzDeliLine.shift()}.`;
   }
-  return "The line is currently empty.";
+  return "There is nobody waiting to be served!";
 }
 
 function currentLine(katzDeliLine) {
